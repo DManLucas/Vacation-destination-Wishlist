014ee673bbce9ad92f0dadf65b3dc2b40a5c86e4
Lucas Nakeli <luminouslucas@duck.com>
2024-07-24 12:14:43 +0200

    Fixed typo in index.html, added functionality to script.js to create destination cards and handle wishlist container

--- script.js
+++ script.js
@@ -13,4 +13,52 @@ function handleFormSubmit(e) {
   for (let i = 0; i < detailsForm.length; i++) {
     detailsForm.element[i].value = "";
   }
+
+  let wishListContainer = document.getElementById("destinations_container");
+  if (wishListContainer.children.length === 0) {
+    document.getElementById("title").innerHTML = "My wishlist";
+  }
+}
+
+function createDestinationCard(name, location, photoURL, description) {
+  let card = document.createElement("div");
+  card.className = "card";
+
+  let img = document.createElement("img");
+  img.setAttribute("alt", name);
+
+  let constantPhotoURL = "images/signpost.jpg";
+  if (photoURL.length === 0) {
+    img.setAttribute("src", constantPhotoURL);
+  } else {
+    img.setAttribute("src", photoURL);
+  }
+  card.appendChild(img);
+
+  let cardBody = document.createElement("div");
+  cardBody.className("card-body");
+
+  let cardTitle = document.createElement("h3");
+  cardTitle.innerText = name;
+  cardBody.appendChild(cardTitle);
+
+  let cardSubtitle = document.createElement("h4");
+  cardSubtitle.innerText = location;
+  cardBody.appendChild(cardSubtitle);
+
+  if (description.length !== 0) {
+    let cardText = document.createElement("p");
+    cardText.className = "card-text";
+    cardText.innerText = description;
+    cardBody.appendChild(cardText);
+  }
+
+  let cardDeleteBtn = document.createElement("button");
+  cardDeleteBtn.innerText = "Remove";
+  cardDeleteBtn.addEventListener("click", removeDestination);
+  cardBody.appendChild(cardDeleteBtn);
+
+  card.appendChild(cardBody);
+
+  return card;
 }
